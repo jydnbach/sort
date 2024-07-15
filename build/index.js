7e0d33dc48e4a47415d3ce7cf4a4c4d0356e6b80
jydnbach <jaydenkim95@gmail.com>
2024-07-15 12:52:22 +0900

    sorting algorithm

--- build/index.js
+++ build/index.js
@@ -2,12 +2,24 @@
 // terminal: tsc index.ts … too much! => tsc --init => config: outdir: './build' rootdir: './src' =>  tsc -w (watches src automatically and builds in build)
 // npm i nodemon concurrently => package.json: set up start commands
 class Sorter {
-    // collection: number[];
+    // collection: number[]; // can delete bc of public
     constructor(collection) {
         this.collection = collection;
-        // this.collection = collection;
+        // this.collection = collection; // can delete bc of public
     }
-    sort() { }
+    sort() {
+        const { length } = this.collection;
+        // sorting algorithm
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
 }
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,26 @@
 // npm i nodemon concurrently => package.json: set up start commands
 
 class Sorter {
-  // collection: number[];
+  // collection: number[]; // can delete bc of public
 
   constructor(public collection: number[]) {
-    // this.collection = collection;
+    // this.collection = collection; // can delete bc of public
   }
 
-  sort(): void {}
+  sort(): void {
+    const { length } = this.collection;
+
+    // sorting algorithm
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
 }
 
 const sorter = new Sorter([10, 3, -5, 0]);
