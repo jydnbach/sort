1c145dcb0a54fbe95656c3af61586ee80743c175
jydnbach <jaydenkim95@gmail.com>
2024-07-15 16:14:20 +0900

    abstract classes

--- build/index.js
+++ build/index.js
@@ -3,12 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // terminal: tsc index.ts … too much! => tsc --init => config: outdir: './build' rootdir: './src' =>  tsc -w (watches src automatically and builds in build)
 // npm i nodemon concurrently => package.json: set up start commands
 const Sorter_1 = require("./Sorter");
-const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedList_1 = require("./LinkedList");
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
-const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
-const sorter = new Sorter_1.Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const sorter = new Sorter_1.Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
