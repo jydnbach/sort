7a61f09e186cad239171f0ada74ffbad6aa6027f
jydnbach <jaydenkim95@gmail.com>
2024-07-15 12:40:14 +0900

    npm start setup

--- build/index.js
+++ build/index.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log('hi'); // terminal: tsc index.ts
+console.log('hi there'); // terminal: tsc index.ts … too much! => tsc --init => config: outdir: './build' rootdir: './src' =>  tsc -w (watches src automatically and builds in build)
+// npm i nodemon concurrently => package.json: set up start commands
+const log = () => {
+    console.log('something');
+};
