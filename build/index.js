a4b31702bbfe22bff2513a9ec671fd752b91b2a8
jydnbach <jaydenkim95@gmail.com>
2024-07-15 13:32:43 +0900

    type guards

--- build/index.js
+++ build/index.js
@@ -12,10 +12,19 @@ class Sorter {
         // sorting algorithm
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                // all of this only works if collection is number[]
+                // if collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // only going to work if collection is a string
+                // if collection is a string, do this logic instead:
+                // compare and swap chars in string
+                if (typeof this.collection === 'string') {
                 }
             }
         }
